67fc658efb3920a2eba93c71e9098930f387c7d5
seethalSpiderWorks <seethal@spiderworks.in>
2024-09-30 11:14:53 +0530

    table changes

--- database/migrations/2024_09_24_034336_create_attributes_table.php
+++ database/migrations/2024_09_24_034336_create_attributes_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('attributes', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('product_id')->default(0);
-            $table->string('slug')->unique();
             $table->string('name');
             $table->integer('priority')->default(0);
             $table->boolean('status')->default(1);
